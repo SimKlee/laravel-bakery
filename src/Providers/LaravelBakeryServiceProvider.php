abe9bb55453fc4bdb58b9af274ffa4d52849bbaf
Simon Kleeschulte <simon.kleeschulte@dr-walter.com>
2021-04-18 09:25:53 +0200

    registered command

--- src/Providers/LaravelBakeryServiceProvider.php
+++ src/Providers/LaravelBakeryServiceProvider.php
@@ -3,17 +3,22 @@
 namespace SimKlee\LaravelBakery\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use SimKlee\LaravelBakery\Console\Commands\BakeModelCommand;
 
 class LaravelBakeryServiceProvider extends ServiceProvider
 {
+    public function boot(): void
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                BakeModelCommand::class,
+            ]);
+        }
+    }
 
     public function register(): void
     {
         //
     }
 
-    public function boot(): void
-    {
-        //
-    }
 }
